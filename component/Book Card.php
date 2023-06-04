93515dde36fef9e658f84aaa63f18b9e1baadddd
AmaraNK9 <naymyo1310mdy@gmail.com>
2023-06-05 00:58:17 +0630

    book card add

---
+++ component/Book Card.php
@@ -0,0 +1,11 @@
+<div class="book-card">
+  <div class="book-image">
+    <img src="book-cover.jpg" alt="Book Cover">
+  </div>
+  <div class="book-details">
+    <h3 class="book-title">Book Title</h3>
+    <p class="book-author">Author Name</p>
+    <p class="book-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vestibulum orci a malesuada malesuada.</p>
+    <a href="#" class="read-review">Read Review</a>
+  </div>
+</div>
